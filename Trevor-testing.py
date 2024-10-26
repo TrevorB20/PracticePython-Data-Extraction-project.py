28ce9ca06933c3cfb08125b9addbf6b591f3b197
TrevorB20 <trevorboodram1289@gmail.com>
2024-10-26 13:23:22 -0400

    Create Trevor-testing.py

---
+++ Trevor-testing.py
@@ -0,0 +1,8 @@
+try:
+    value = int(input('Enter an integer: 0'))
+    print('The inverse of', value, 'is', 1/value)
+except:
+    print('You Did Not Provie me with a value, so this will not Work!!')
+
+value = int(input('Enter an integer: '))
+print('The inverse of',value, 'is', 1/value)
