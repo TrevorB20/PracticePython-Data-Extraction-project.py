f9e8a2a4e117c2d75d46478ec25ea2011079ab60
TrevorB20 <trevorboodram1289@gmail.com>
2024-12-06 13:52:59 -0500

    Mistake

--- tuble-lists.py
+++
@@ -1,7 +0,0 @@
-city_1 = ('London', 'UK', 8.98)
-city_2 = ('Algiers', 'Algeria', 3.9)
-city_3 = ('Canberra', 'Australia', 0.4)
-capitals = [('London', 'UK', 8.98), ('Algiers', 'Algeria', 3.9), ('Canberra', 'Australia', 0.4)]
-for capital in capitals:
-    print('Name:', capital[0], ', Country:', capital[1], ', Population:', capital[2])
-    
